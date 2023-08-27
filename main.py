ec1fdbb0c5c9d0c7018fa7587accfc8f2756da69
MaxDLink <max.durham.link@gmail.com>
2023-08-26 22:21:38 -0500

    updated TODOS

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import chainlit as cl 
 import openai 
 import os 
-#TODO - 48:43 into 2 hour video - use langchain for next section. 
 os.environ['OPENAI_API_KEY'] = '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'
 openai.api_key = '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'
 #return everything that the user inputs 
